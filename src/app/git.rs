929afdf8c5fcbbec16e05e8293ba8fa0d08e423f
Jinghui Hu <hujinghui@buaa.edu.cn>
2026-03-04 20:59:01 +0800

    fix(git): 使用 run_quiet 替代 run 以静默执行 git 命令
    避免在获取版本标签时产生不必要的输出干扰

--- src/app/git.rs
+++ src/app/git.rs
@@ -2,7 +2,7 @@ use super::runner::CommandRunner;
 use super::version::compare_versions;
 
 pub fn get_current_version() -> Option<String> {
-    let output = CommandRunner::run("git", &["tag", "-l", "v*"]).ok()?;
+    let output = CommandRunner::run_quiet("git", &["tag", "-l", "v*"]).ok()?;
 
     let tags = String::from_utf8(output.stdout).ok()?;
     let mut tags: Vec<&str> = tags.lines().collect();
