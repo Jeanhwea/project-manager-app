9f95f8a349f533afcd6a0368f8944ef42631a565
Jinghui Hu <hujinghui@buaa.edu.cn>
2026-04-22 12:41:50 +0800

    v1.0.1

--- Formula/pma.rb
+++ Formula/pma.rb
@@ -1,7 +1,7 @@
 class Pma < Formula
   desc "Git repository batch management tool"
   homepage "https://github.com/Jeanhwea/project-manager-app"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
 
   on_macos do
